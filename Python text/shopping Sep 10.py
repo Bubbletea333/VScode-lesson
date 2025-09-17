a999684e3c3aabb74e579439bc8bffd644b3f490
Bubbletea333 <vienna.gracella@gmail.com>
2025-09-17 16:03:35 +0700

    committed

--- Python text/shopping Sep 10.py
+++ Python text/shopping Sep 10.py
@@ -1,11 +1,12 @@
 print('Welcome to online shopping🛒🛍️')
 item=str(input("\nplease enter the item you want to buy: "))
 much=int(input(f"How much {item} would you want to buy?: "))
+each=int(input(f"How much is for one?(in dollars): "))
 member=(input("are you a member?(true/false): "))
 if member=='true':
     float(input(f"what is your member ID: "))
 elif member=='false':
-    member1=bool(input(f"would you like to be a member in online shopping?(true/false): "))
+    member1=str(input(f"would you like to be a member in online shopping?(true/false): "))
     if member1=='true':
       str(input(f"please enter your phone number: "))
       print(input("here is your membership and this is your ID(press enter)"))
@@ -15,4 +16,8 @@ elif member=='false':
 
 #continue to create discount system
    
-   
+print("---Recipt---")
+print(f"item:{item}")
+print(f"you bought {much} {item}")
+subtotal=(much*each)
+print(f"subtotal: ({much}*{each})=${subtotal}")
