e8581347a3ffbe794f883e4f00b912d2f6a3bfa9
Bubbletea333 <vienna.gracella@gmail.com>
2025-09-25 11:58:08 +0700

    commit

--- Python text/string operations Sept 25.py
+++ Python text/string operations Sept 25.py
@@ -1,5 +1,10 @@
 print("hello player")
-#ssfirst_name=(input("What is your name?: "))
-#print(f"hello {first_name}")
-#last_name=(input("What is your last name?: "))
-#print("your name is "+first_name+" "+last_name)
+first_name=(input("What is your name?: "))
+print(f"hello {first_name}")
+last_name=(input("What is your last name?: "))
+print("your name is "+first_name+" "+last_name)
+
+hobby=(input("what is your hobby?: ")).upper()
+print(f"your hobby is {hobby}")
+
+print("your hobby has ", len(hobby),"letters")
