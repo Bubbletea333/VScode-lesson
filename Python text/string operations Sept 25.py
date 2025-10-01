3a2a4b8f131b0bd09b100c07630607b3cb639f96
Bubbletea333 <vienna.gracella@gmail.com>
2025-10-01 16:01:21 +0700

    commit

--- Python text/string operations Sept 25.py
+++ Python text/string operations Sept 25.py
@@ -8,3 +8,10 @@ hobby=(input("what is your hobby?: ")).upper()
 print(f"your hobby is {hobby}")
 
 print("your hobby has ", len(hobby),"letters")
+
+word=(input("please enter a word: "))
+print("Does you're word starts with 's'?", word.lower().startswith("a"))#we put lower so when the player write 's' in the start it will be true but if we put 'S' in capital it will be false.
+
+sentence=(input("now try to write a sentence: "))
+new_sentence = sentence.replace("s", "$")
+print("Here is your sentence with 's' replaced by'$':", new_sentence)
