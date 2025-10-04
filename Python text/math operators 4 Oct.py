22cd92e356e75da01245d5ea60c0c3d9d7039191
Bubbletea333 <vienna.gracella@gmail.com>
2025-10-04 11:56:43 +0700

    commit

---
+++ Python text/math operators 4 Oct.py
@@ -0,0 +1,10 @@
+first_number=int(input("enter your first number: "))
+second_number=int(input("enter your second number: "))
+
+print(f"addition:{first_number+second_number}")
+print(f"subtraction:{first_number-second_number}")
+print(f"multiplication:{first_number*second_number}")
+print(f"division:{first_number/second_number}")
+print(f"modulus:{first_number%second_number}")#modulus is like a remainder after you divide the numbers
+print(f"exponentiation:{first_number**second_number}")#exponentiation is like the power of
+print(f"floor division:{first_number//second_number}")#the floor division // rounds the result down to the nearest whole number
