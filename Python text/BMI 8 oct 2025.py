36cf08b139524789f011a1e9df665894861c6118
Bubbletea333 <vienna.gracella@gmail.com>
2025-10-08 15:50:00 +0700

    commit

---
+++ Python text/BMI 8 oct 2025.py
@@ -0,0 +1,16 @@
+height=(input("what is your height in centimeters= "))
+weight=(input("what is your weight in kilograms= "))
+
+height=(height/100)
+bmi=(weight/(height*height))
+
+if bmi <= 16:
+    print("you are severely underweight")
+elif bmi <= 18.5:
+    print("you are underweight")
+elif bmi <= 25:
+    print("you are healthy")
+elif bmi >= 30:
+    print("severely overweight")
+else :
+    print("fill the height and weight properly")
