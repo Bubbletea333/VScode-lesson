c3c4963ff94c3bf1e802ed8cd1136779d19b2169
Bubbletea333 <vienna.gracella@gmail.com>
2025-10-22 15:57:07 +0700

    commit

--- Python text/BMI 8 oct 2025.py
+++ Python text/BMI 8 oct 2025.py
@@ -3,14 +3,15 @@ weight=int(input("what is your weight in kilograms= "))
 
 height=(height/100)
 bmi=(weight/(height*height))
+print(bmi)
 
-if bmi <= 16:
+if bmi <= 16 and bmi<18:
     print("you are severely underweight")
-elif bmi <= 18.5:
+elif bmi <= 18.5 and bmi<24:
     print("you are underweight")
-elif bmi <= 25:
+elif bmi <= 25 and bmi<29:
     print("you are healthy")
-elif bmi >= 30:
+elif bmi >= 30 and bmi<50:
     print("severely overweight")
 else :
     print("fill the height and weight properly")
---
+++ Python text/Calculator 22 Oct 2025.py
@@ -0,0 +1,6 @@
+first_number=int(input("enter your first number: "))
+second_number=int(input("enter your second number: "))
+simbols=(input("choose operations +,-,%,*,/,**,//: "))
+
+if simbols == "+":
+    {first_number+second_number}
