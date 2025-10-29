d11004a4995ddbd2df79368b0cd3a1c0cab6580b
Bubbletea333 <vienna.gracella@gmail.com>
2025-10-29 15:42:07 +0700

    commit

--- Python text/Calculator 22 Oct 2025.py
+++ Python text/Calculator 22 Oct 2025.py
@@ -3,4 +3,18 @@ second_number=int(input("enter your second number: "))
 simbols=(input("choose operations +,-,%,*,/,**,//: "))
 
 if simbols == "+":
-    {first_number+second_number}
+    print(first_number+second_number)
+elif simbols == "-":
+    print(first_number-second_number)
+elif simbols == "%":
+    print(first_number%second_number)
+elif simbols == "*":
+    print(first_number*second_number)
+elif simbols == "/":
+    print(first_number/second_number)
+elif simbols == "**":
+    print(first_number**second_number)
+elif simbols == "//":
+    print(first_number//second_number)
+else:
+    print("enter correct simbol")
