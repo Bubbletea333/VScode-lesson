2e5536d08bdf5e22ad7d062a69fac70b37386c24
Bubbletea333 <vienna.gracella@gmail.com>
2025-11-12 15:59:56 +0700

    commit

--- Python text/travel planner 8 Nov 2025.py
+++ Python text/travel planner 8 Nov 2025.py
@@ -1,11 +1,18 @@
 destination=str(input("where do you want to go (beach/mountain): ")).lower()
 weather=str(input("what kind of weather do you like(sunny/rainy):  ")).lower()
-budget=float(input("what is your budget for the destination (Rp): "))
+budget=float(input("what is your budget for the destination (Rp.20000-100000): "))
 
-if budget <= 35000:
+if budget >= 20000 and budget<= 35000:
     print("try to do something in the main city")
     if destination =="beach":
         if weather =="sunny":
             print("try do to surfing")
-elif weather =="rainy":
-    print("try to sit and relax at the nearest cafe")
+        elif weather =="rainy":
+            print("try to sit and relax at the nearest cafe")
+    if destination =="mountain":
+        if weather =="sunny":
+            print("try to make s'mores")
+        elif weather =="rainy":
+            print("try to read a book")
+elif budget >35000 and budget >= 50000 and budget<= 100000:
+    print("")
