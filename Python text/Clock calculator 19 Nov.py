5ba5a54d0c30202f6833ba7d697c9a042d9e159f
Bubbletea333 <vienna.gracella@gmail.com>
2025-11-19 15:57:46 +0700

    commit

---
+++ Python text/Clock calculator 19 Nov.py
@@ -0,0 +1,3 @@
+hour=int(input("hours later if now is 7= "))
+hours=(7+hour)%12
+print(f"so it will be {hours} o'clock")
---
+++ Python text/Day of the week 19 Nov.py
@@ -0,0 +1,3 @@
+day=int(input("if today is Monday,how many days do you want to skip= "))
+days=(1+day)%7#I should pay attention at the sequence of operations
+print(f"so it will be day {days}")#I should remember to use print in order to make the sentence appear
