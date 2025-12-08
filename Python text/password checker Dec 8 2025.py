27990604316c831aa14252315acdd6cecf353879
Bubbletea333 <vienna.gracella@gmail.com>
2025-12-08 14:02:06 +0700

    commit

---
+++ Python text/password checker Dec 8 2025.py
@@ -0,0 +1,5 @@
+password=""
+while password !="banana3":
+    password = (input("write your password: "))
+
+print("--ACCESS GRANTED--")
---
+++ Python text/yesno validator Dec 8 2025.py
@@ -0,0 +1,5 @@
+choose=(input("do you want to continue?(yes/no): ")).lower()
+while choose not in ["no","yes"]:#list is a kind of variable that stores multiple items 
+    print("please write yes or no")
+
+print("ok")
