60e388939e84b513bccbdb116e9152906d0154f8
Bubbletea333 <vienna.gracella@gmail.com>
2025-12-10 15:21:22 +0700

    commit

--- Python text/yesno validator Dec 8 2025.py
+++ Python text/yesno validator Dec 8 2025.py
@@ -1,5 +1,5 @@
 choose=(input("do you want to continue?(yes/no): ")).lower()
 while choose not in ["no","yes"]:#list is a kind of variable that stores multiple items 
-    print(input("please write yes or no"))
+    choose=(input("INVALiD..... please write yes or no: "))
     
-print("ok")
+print("ok thank you for the confirmation")
