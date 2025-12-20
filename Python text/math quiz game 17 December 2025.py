f1b0625f031a5eca4b0e7a2eb1924a3f1032a2e1
Bubbletea333 <vienna.gracella@gmail.com>
2025-12-20 11:43:36 +0700

    commit

--- Python text/math quiz game 17 December 2025.py
+++ Python text/math quiz game 17 December 2025.py
@@ -1,9 +1,14 @@
 import random
 score=0
-a=(random.randint(1, 20))
-b=(random.randint(1, 20))
-answer=(input(f"what is {a}*{b}= "))
-if answer == a*b:
-    print("correct!")
-else:
-    print("incorrect")
+for i in range(10):#I need to put the for loop here so the a and b can change the numbers every time it loops
+    a=(random.randint(1, 10))
+    b=(random.randint(1, 10))
+    answer=int(input(f"what is {a}*{b}= "))#I need to put the int data type so it doesn't
+#count numbers as string
+    if answer == a*b:
+        print("correct!")
+        score=score+1
+        print(f"score:{score}")
+    else:
+        print("incorrect!")
+        print(f"score:{score}")
