18f592b3de0c5fe80d8a2e321cbd6c68b8156f3e
Bubbletea333 <vienna.gracella@gmail.com>
2026-01-10 11:52:52 +0700

    commit

--- Python text/guessing number game Jan 7 2026.py
+++ Python text/guessing number game Jan 7 2026.py
@@ -1,11 +1,15 @@
 import random
 
 guess=0
-number=(random.randint(1, 50))
-guess=int(input("i have a random number between 1 until 50 can you try to guess?"))
-
-while guess != number:
+number=(random.randrange(1, 100))
+guess=int(input("i have a random number between 1 until 100 can you try to guess?"))
+while guess != number:    
     if guess > number:
-        print(input("lower than that!"))
+        print("lower than that!")
+        guess=int(input("try again: "))
     elif guess < number:
-        print(input("higher than that!"))
+        print("higher than that!")
+        guess=int(input("try again: "))
+    else:
+        break
+print("BINGO")
