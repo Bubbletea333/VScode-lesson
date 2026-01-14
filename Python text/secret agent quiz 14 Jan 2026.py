71e985530386fdb0cdbed0c0330a08496f84e738
Bubbletea333 <vienna.gracella@gmail.com>
2026-01-14 15:58:31 +0700

    commit

---
+++ Python text/secret agent quiz 14 Jan 2026.py
@@ -0,0 +1,13 @@
+correct_name="Agent Amber"
+correct_code="Expedocious endorphins"
+access_granted=False
+
+print("you are in the area of the secret rooms")
+while correct_name != "Agent Amber":
+    correct_name=(input("what is your name? "))
+    print("wrong Agent")
+    print("your room is beside me")
+    print("or your just trying to get in my room")
+else:
+    print("hello Agent Amber")
+    correct_code=(input("What is the secret password: "))
