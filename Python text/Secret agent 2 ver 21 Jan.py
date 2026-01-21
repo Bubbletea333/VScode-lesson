075a999a4d9203a7335f78718603f7784bd156f7
Bubbletea333 <vienna.gracella@gmail.com>
2026-01-21 15:55:59 +0700

    commit

---
+++ Python text/Secret agent 2 ver 21 Jan.py
@@ -0,0 +1,13 @@
+correct_passcode="syronevolcanicexpedocius"
+correct_number="8373"
+access_granted=False
+
+print("you are in the area of the one and only dangerous room")
+print("you are in front of agent spoken's room")
+
+correct_passcode=(input("what is your passcode? "))
+while correct_passcode != "syronevolcanicexpedocious":
+    print(input("wrong passcode: "))
+else:
+    access_granted=True
+    access_granted=(input("What is your password"))
--- Python text/secret agent quiz 14 Jan 2026.py
+++ Python text/secret agent quiz 14 Jan 2026.py
@@ -1,13 +1,19 @@
 correct_name="Agent Amber"
 correct_code="Expedocious endorphins"
-access_granted=False
 
 print("you are in the area of the secret rooms")
+print("you are in front of agent Amber's room")
+
+correct_name=(input("what is your name? "))
 while correct_name != "Agent Amber":
-    correct_name=(input("what is your name? "))
     print("wrong Agent")
-    print("your room is beside me")
-    print("or your just trying to get in my room")
+    print("probably your room is beside me")
+    correct_name=(input("what is your name? "))
 else:
     print("hello Agent Amber")
     correct_code=(input("What is the secret password: "))
+while correct_code!="Expedocious endorphins":
+    print("wrong answer")
+    correct_code=(input("What is the secret password: "))
+else:
+    print("ACCESS GRANTED")
