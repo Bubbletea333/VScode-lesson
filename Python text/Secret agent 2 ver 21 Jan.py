fd1991c331c3581b781bc645b458ced5ae00faf8
Bubbletea333 <vienna.gracella@gmail.com>
2026-01-28 16:01:48 +0700

    commit

--- Python text/Secret agent 2 ver 21 Jan.py
+++ Python text/Secret agent 2 ver 21 Jan.py
@@ -1,13 +1,17 @@
 correct_passcode="syronevolcanicexpedocius"
-correct_number="8373"
+correct_number=8373
 access_granted=False
 
 print("you are in the area of the one and only dangerous room")
 print("you are in front of agent spoken's room")
 
-correct_passcode=(input("what is your passcode? "))
-while correct_passcode != "syronevolcanicexpedocious":
-    print(input("wrong passcode: "))
-else:
-    access_granted=True
-    access_granted=(input("What is your password"))
+while access_granted == False:
+    correct_passcode=str(input("what is the passcode: "))
+    if correct_passcode == "syronevolcanicexpedocius":
+        print("correct")
+        correct_number=int(input("WHAT IS THE CORRECT NUMBER: "))
+        if correct_number == 8373:
+            access_granted == True
+            print("you are inside the room")
+#1. make this program more friendly
+#2. FOCUS FOCUS FOCUS
