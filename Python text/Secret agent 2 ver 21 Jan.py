147a272e4acc50225f3711f059b6ebcb979d3a09
Bubbletea333 <vienna.gracella@gmail.com>
2026-01-31 11:42:48 +0700

    commit

--- Python text/Secret agent 2 ver 21 Jan.py
+++ Python text/Secret agent 2 ver 21 Jan.py
@@ -9,9 +9,14 @@ while access_granted == False:
     correct_passcode=str(input("what is the passcode: "))
     if correct_passcode == "syronevolcanicexpedocius":
         print("correct")
-        correct_number=int(input("WHAT IS THE CORRECT NUMBER: "))
+        correct_number=int(input("What is the correct number: "))
         if correct_number == 8373:
             access_granted == True
             print("you are inside the room")
+            break
+        else:
+            correct_number=int(input("Wrong,try the number again...: "))
+    else:
+        correct_passcode=str(input("Wrong,try the passcode again...: "))
 #1. make this program more friendly
 #2. FOCUS FOCUS FOCUS
