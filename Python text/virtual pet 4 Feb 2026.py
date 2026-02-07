50538cad6a0da9f3780cb1c9a95ce1dac4f710d6
Bubbletea333 <vienna.gracella@gmail.com>
2026-02-07 11:56:00 +0700

    commit

--- Python text/virtual pet 4 Feb 2026.py
+++ Python text/virtual pet 4 Feb 2026.py
@@ -1,5 +1,38 @@
 pet_name = "Sparkle"
-hunger="5/10"
-energy="5/10"
-happiness="5/10"
+hunger=5
+energy=5
+happiness=5
 keep_alive=True
+
+print("Welcome to the digital Pet Hub")
+print("keep sparkle healthy")
+
+while keep_alive == True:
+    print("\nSparkle's status:")
+    print(f"hunger:{hunger}/10")
+    print(f"energy:{energy}/10")
+    print(f"happiness:{happiness}/10")
+
+    print("\nWhat would Sparkle do?")
+    print("1.Eat 2.Sleep 3.Play")
+    pick=(input("pick 1 , 2 or 3 :"))
+
+    if pick == 1:
+        hunger=hunger+2
+        print("Sparkle is eating now")
+
+    elif pick == 2:
+        energy=energy+2
+        happiness=happiness-1
+        print ("Sparkle is sleeping, shh be quiet")
+
+    elif pick == 3:
+        energy=energy-2
+        hunger=hunger-1
+        happiness=happiness+2
+        print("Sparkle played catch")
+    
+    else:
+        print(input("please write 1 2 or 3: "))
+
+        #display the status
