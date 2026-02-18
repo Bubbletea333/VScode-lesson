fc66d6e9c129052b2fd5ab62b8fe0d380e963d92
Bubbletea333 <vienna.gracella@gmail.com>
2026-02-18 15:56:16 +0700

    commit

--- Python text/virtual pet 4 Feb 2026.py
+++ Python text/virtual pet 4 Feb 2026.py
@@ -13,33 +13,45 @@ while keep_alive == True:
     print(f"energy:{energy}/10")
     print(f"happiness:{happiness}/10")
 
-    print("\nWhat would Sparkle do?")
-    print("1.Eat 2.Sleep 3.Play")
-    pick=str(input("pick 1 , 2 or 3 :"))#Python consideres the variable value as a string IF we dont type any Data types
+    
 
-    if pick == "1":
-        hunger=hunger+2
-        print("Sparkle is eating now")
-
-    elif pick == "2":
-        energy=energy+2
-        happiness=happiness-1
-        print ("Sparkle is sleeping, shh be quiet")
-
-    elif pick == "3":
-        energy=energy-2
-        hunger=hunger-1
-        happiness=happiness+2
-        print("Sparkle played catch")
-
-    elif energy <= 0 or hunger <= 0 or happiness <= 0:
+    if energy <= 0 or hunger <= 0 or happiness <= 0:#Checking for game over
         keep_alive==False
-        print("oh no poor Sparkle")
+        print("oh no poor Sparkle（；´д｀）ゞ")
+        print("GAME OVER(┬┬﹏┬┬)")
         break
 
-    else:
-        print(str(input("please write 1 2 or 3: ")))
 
-if keep_alive == False:
-    print("oh nooooooooo...... (っ °Д °;)っSparkle (┬┬﹏┬┬)")
+    else:
+        print("\nWhat would Sparkle do?")
+        print("1.Eat 2.Sleep 3.Play")
+        pick=str(input("pick 1 , 2 or 3 :"))#Python consideres the variable value as a string IF we dont type any Data types
+        
+        if pick == "1":
+            hunger=hunger+2
+            print("Sparkle is eating now")
+
+        elif pick == "2":
+            energy=energy+2
+            happiness=happiness-1
+            print ("Sparkle is sleeping, shh be quiet")
+
+        elif pick == "3":
+            energy=energy-2
+            hunger=hunger-1
+            happiness=happiness+2
+            print("Sparkle played catch")
+
+            if energy > 10:
+                energy= 10
+            
+            elif hunger > 10:
+                hunger= 10
+
+            elif happiness > 10:
+                happiness= 10
+
+        else:
+            print("INVALID"+ "please write 1 or 2 or 3")
+
         #do check how to break the loop
