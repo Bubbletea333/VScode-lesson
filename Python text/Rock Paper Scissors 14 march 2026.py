71175fd2122b8216ee52eb0d6b53f87070c57417
Bubbletea333 <vienna.gracella@gmail.com>
2026-03-28 11:54:21 +0700

    commit

--- Python text/Rock Paper Scissors 14 march 2026.py
+++ Python text/Rock Paper Scissors 14 march 2026.py
@@ -13,7 +13,38 @@ print("The loser needs to do something embarrassing for 30 seconds")
 print("so let's the show begin")
 
 while player_score <= 3 and computer_score <= 3:
+    print(f"player score = {player_score}   enemy score = {computer_score}")
     print("\nwhat will you pick")
-    player=(input("rock paper or scissors= "))
+    player=(input("rock paper or scissors= ")).lower()
 
-    if player 
+    if player =="rock":
+        computer=print(random.choices(choices))
+        if computer=="paper":
+            computer_score = computer_score+1
+        elif computer=="scissors":
+            player_score = player_score+1
+        else:
+            print("it's a tie")
+
+    elif player =="paper":
+        computer=print(random.choices(choices))
+        if computer=="scissors":
+            computer_score = computer_score+1
+        elif computer=="rock":
+            player_score = player_score+1
+        else:
+            print("it's a tie")
+
+    elif player =="scissors":
+        computer=print(random.choices(choices))
+        if computer=="rock":
+            computer_score = computer_score+1
+        elif computer=="paper":
+            player_score = player_score+1
+        else:
+            print("it's a tie")
+
+    else:
+        print("please write it correctly")
+
+    
