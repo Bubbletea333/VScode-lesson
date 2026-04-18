c811a7629175ea304c04f0a321407248ca8471fe
Bubbletea333 <vienna.gracella@gmail.com>
2026-04-18 11:41:30 +0700

    commit

--- Python text/Magical Backpack 11/4/26.py
+++ Python text/Magical Backpack 11/4/26.py
@@ -8,7 +8,7 @@ in_danger= True
 while in_danger==True:
     print(f"\nyour backpack contains: {magical_backpack}")
     print("\nwhat will you do")
-    print("1. try to find the correct key and put it in your backpack")
+    print("1. try to find the 'Correct Key' and put it in your backpack")
     print("2. throw something from your backpack")
     print("3. try to open the door")
     print("4. give up and will be there for 100 years")
@@ -18,3 +18,29 @@ while in_danger==True:
         pick=(input("what did you find: "))
         magical_backpack.append(pick)
         print(f"you inserted {pick} into your magical backpack")
+
+    elif player =="2":
+        throw=(input("what will you throw?= "))
+        if throw in magical_backpack:            
+            magical_backpack.remove(throw)
+            print(f"you threw {throw} from your magical backpack")
+        else:
+            print(f"\nlooks like there are no {throw}🤔")
+
+    elif player =="3":
+        if "Correct Key" in magical_backpack:
+            in_danger=False
+            print("you heard a click sound")
+            print("yay you unlocked the front door")
+            break
+        else:
+            print("the door is still locked")
+
+    elif player =="4":
+        print("you stayed there for 100 years")
+        break
+
+    else:
+        print("please write it correctly😡")
+
+        
