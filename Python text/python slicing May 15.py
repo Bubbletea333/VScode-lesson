857e4a6f932e339582a3e31a6b01cdd86891238a
Bubbletea333 <vienna.gracella@gmail.com>
2026-05-15 14:35:39 +0700

    commit

---
+++ Python text/python slicing May 15.py
@@ -0,0 +1,71 @@
+#🟢 Level 1 — Easy
+#Exercise 1
+colors = ["red", "blue", "green", "yellow", "purple"]
+
+#👉 Print:
+
+#first 2 colors
+#last 2 colors
+print(colors[0:2])
+print(colors[3:5])
+
+#Exercise 2
+animals = ["cat", "dog", "bird", "fish"]
+
+#👉 Print only:
+
+#['dog', 'bird']
+
+print(animals[1:3])
+
+#🟡 Level 2 — Medium
+#Exercise 3
+foods = ["pizza", "burger", "fries", "ice cream", "cake"]
+
+#👉 Print:
+
+#first 3 foods
+#everything after index 2
+
+print(foods[2:5])
+
+#Exercise 4
+numbers = [10, 20, 30, 40, 50, 60]
+
+#👉 Print:
+
+#middle 3 numbers
+print(numbers[1:4])
+
+
+#🔴 Level 3 — Challenge
+#Exercise 5
+
+#Create your own list of:
+
+#6 games 🎮
+
+#Then:
+
+#print first 2 games
+#print last 3 games
+#print middle games
+
+games=["Minecraft","Roblox","Genshin Impact","Pacman","Pokemon","Tomodachi Life"]
+print(games[0:3])
+print(games[3:6])
+print(games[2:4])
+
+#Exercise 6
+pets = ["Sparkle", "Cookie", "Fluffy", "Milo", "Luna"]
+
+#👉 Make a NEW list called:
+
+#small_group
+
+#that contains only:
+
+#['Cookie', 'Fluffy', 'Milo']
+
+Small_group=[(pets[1:4])]
+print(Small_group)
