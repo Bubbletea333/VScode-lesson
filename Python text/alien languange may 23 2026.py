30d2f2dfe7729af0ce4e2ac506ab7866a0326d05
Bubbletea333 <vienna.gracella@gmail.com>
2026-05-23 12:04:18 +0700

    commit

---
+++ Python text/alien languange may 23 2026.py
@@ -0,0 +1,8 @@
+language=["----","sjgfjskhk","we","like","food","give","us","pizza","jdsfgdfjhewgjfg"]
+print("---you got a message from aliens---")
+print(language)
+print("\n ---changing language---")
+print(language[2:8])
+print("\n ---words we dont need---")
+print(language[:2])
+print(language[8:])
---
+++ Python text/battle may 23 2026.py
@@ -0,0 +1,11 @@
+import random
+weapons=["staff","lighting bolt","dagger","fireball","1000 wolfs","bow and arrow"]
+enemy_hp=200
+enemy_choose=""
+
+print("you are fighting the final boss")
+print(weapons)
+print("these are your weapons")
+choose=(input("\n would you like to do a combo or not?= ")).lower
+if choose == "yes":
+    print(input("pick 0 until 5="))
