ac36bf43841a69a22c9292bbae7b511f10e6500e
Bubbletea333 <vienna.gracella@gmail.com>
2026-05-30 12:00:15 +0700

    commit

--- Python text/battle may 23 2026.py
+++ Python text/battle may 23 2026.py
@@ -1,11 +1,26 @@
 import random
-weapons=["staff","lighting bolt","dagger","fireball","1000 wolfs","bow and arrow"]
+weapon_storage=["staff","bow and arrow","dagger","fireball","1000 wolfs","lightning bolt","super power","ultimate bomb","1 dragon"]
 enemy_hp=200
 enemy_choose=""
+player_hp=200
 
 print("you are fighting the final boss")
-print(weapons)
-print("these are your weapons")
-choose=(input("\n would you like to do a combo or not?= ")).lower
-if choose == "yes":
-    print(input("pick 0 until 5="))
+print(f"final boss hp{enemy_hp}")
+print(f"your hp{player_hp}")
+choose=(input(str("\n choose your weapons junk, intermediate, advanced= "))).lower()
+if choose == "junk":
+    player_weapon=weapon_storage[0:3]
+    print("\nyou have")
+    print(player_weapon)
+elif choose=="intermediate":
+    player_weapon=weapon_storage[3:7]
+    print("\nyou have")
+    print(player_weapon)
+elif choose=="advanced":
+    player_weapon=weapon_storage[7:10]
+    print("\nyou have")
+    print(player_weapon)
+else:
+    print("it is not in the selection")
+   # while enemy_hp >0 or player_hp >0:
+
