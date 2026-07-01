d9da87975ad8bbf2314380dc3500563f2d6085b5
Bubbletea333 <vienna.gracella@gmail.com>
2026-07-01 10:45:54 +0700

    commit

---
+++ Python text/robot brain 1 July 2026.py
@@ -0,0 +1,12 @@
+print("welcome")
+print ("you are standing near a robot")
+
+robotbrain={
+    "hello" : "hi I'm a robot",
+    "joke"  : "Why did the banana go to the doctor? Because it wasn't peeling well!",
+    "name"  : "my name is 16385au",
+    "moon"  : "Because the Moon has no wind or weather, the footprints left by Apollo astronauts will stay there for millions of years.",
+    "bye"   : "bye have a great day",
+}
+#while 
+print(input("try to say something: ")).lower()
