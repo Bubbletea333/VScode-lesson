f787dd394b662e938f0b8ec107fa9918eb7e40b2
Bubbletea333 <vienna.gracella@gmail.com>
2026-07-18 11:56:07 +0700

    commit

---
+++ Python text/monster encyclopedia 18 Juli 2026.py
@@ -0,0 +1,59 @@
+print(input("type a pokemon: "))
+
+pokedex={
+    "Pikachu" : {
+        "Total":430,
+        "HP":45,
+        "Attack":80,
+        "Defense":50,
+        "Sp.Atk":75,
+        "Sp.def":60,
+        "Speed":120
+    },
+    "Muk" : {
+        "Total":500,
+        "HP":105,
+        "Attack":105,
+        "Defense":75,
+        "Sp.Atk":65,
+        "Sp.def":100,
+        "Speed":50
+    },
+    "Golem" : {
+        "Total":495,
+        "HP":80,
+        "Attack":120,
+        "Defense":130,
+        "Sp.Atk":55,
+        "Sp.def":65,
+        "Speed":45
+    },
+    "Zapdos" : {
+        "Total":580,
+        "HP":90,
+        "Attack":90,
+        "Defense":85,
+        "Sp.Atk":125,
+        "Sp.def":90,
+        "Speed":100
+    },
+    "Gengar" : {
+        "Total":500,
+        "HP":60,
+        "Attack":65,
+        "Defense":60,
+        "Sp.Atk":130,
+        "Sp.def":75,
+        "Speed":110
+    },
+    "Relicanth" : {
+        "Total":485,
+        "HP":100,
+        "Attack":90,
+        "Defense":130,
+        "Sp.Atk":45,
+        "Sp.def":65,
+        "Speed":55
+    },
+}
+#https://pokemondb.net/pokedex/all
